c1c95b2ba834680a99f0682be84cc5c51033cd41
Daniel Olson <emaildano@gmail.com>
2021-01-06 20:37:28 -0500

    updated theme js

--- functions.php
+++ functions.php
@@ -14,11 +14,13 @@
 function galaxy_enqueue_scripts()
 {
 	wp_enqueue_style(
-		'galaxy-wp-theme',
+		'galaxy-wp-theme-styles',
 		get_stylesheet_directory_uri() . '/style.css',
 		[],
 		'0.0.0'
 	);
+
+	wp_enqueue_script('galaxy-wp-theme-scripts', get_theme_file_uri() . '/main.js', array(), '0.0.0', true);
 
 	wp_dequeue_style('hello-elementor');
 	wp_dequeue_style('hello-elementor-theme-style');
