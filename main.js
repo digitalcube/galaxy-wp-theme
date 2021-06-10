e43448ce3f5ee3a397ae0c47d302a623013cec6f
Daniel Olson <emaildano@gmail.com>
2021-06-10 16:38:32 -0400

    removed showcase js from mina.main.js

--- main.js
+++ main.js
@@ -23,19 +23,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
 });
 
-// Showcase
-document.addEventListener("DOMContentLoaded", function (event) {
-  let link = document.querySelectorAll(".showcase .elementor-post a");
-  link.forEach((item) => {
-    item.removeAttribute("href");
-
-    var e = item;
-    var d = document.createElement("span");
-    d.innerHTML = e.innerHTML;
-    e.parentNode.replaceChild(d, e);
-  });
-});
-
 // Post Info
 document.addEventListener("DOMContentLoaded", function (event) {
   let link = document.querySelectorAll(".elementor-post-info a");
